4d613f0834afc5b58c0a350031a242477d66593e
Sebastiano Lucarelli <sebastiano.lucarelli@studio.unibo.it>
2024-02-07 18:21:11 +0100

    added part of methods of the monster interface

--- src/main/java/it/unibo/towermon/model/Monster.java
+++ src/main/java/it/unibo/towermon/model/Monster.java
@@ -2,6 +2,20 @@ package it.unibo.towermon.model;
 
 public interface Monster {
 
-    boolean isFainted();
+    /**
+     * dice se è vivo
+     * @return true if the monster is alive (he have more than 0 HP)
+     * false otherwise
+     */
+    boolean isFainted();     
 
+    String getName();
+
+    int getactualHP();
+
+    int getMaxHP();
+
+    int getAttack();
+
+    int getDefense();
 }
