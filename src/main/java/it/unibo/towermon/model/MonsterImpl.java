4276030163a6fe74d0d1cf46e8d16239f34f868a
Sebastiano Lucarelli <sebastiano.lucarelli@studio.unibo.it>
2024-02-07 18:39:16 +0100

    create class MonsterImpl

---
+++ src/main/java/it/unibo/towermon/model/MonsterImpl.java
@@ -0,0 +1,42 @@
+package it.unibo.towermon.model;
+
+public class MonsterImpl implements Monster
+{
+
+    @Override
+    public boolean isFainted() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isFainted'");
+    }
+
+    @Override
+    public String getName() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getName'");
+    }
+
+    @Override
+    public int getactualHP() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getactualHP'");
+    }
+
+    @Override
+    public int getMaxHP() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getMaxHP'");
+    }
+
+    @Override
+    public int getAttack() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getAttack'");
+    }
+
+    @Override
+    public int getDefense() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getDefense'");
+    }
+
+}
