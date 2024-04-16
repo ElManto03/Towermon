1c4a611698c0d610017788c7974297c94e2e9bb9
Your Real Name <elena.neri8@studio.unibo.it>
2024-04-16 14:42:29 +0200

    prova di commit

---
+++ src/main/java/it/unibo/towermon/view/InitialView.java
@@ -0,0 +1,13 @@
+package it.unibo.towermon.view;
+import javax.swing.*;
+
+public class InitialView extends JFrame {
+    
+    public InitialView(){
+        this.setTitle("Towermon");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.getPreferredSize();
+        this.getpr 
+    }
+}
